bdbd3810b38a2a3d0218b348f9bd5d677d7aef6d
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-04 16:25:49 +0300

    Added scoring system and boudaries to the game

--- game.js
+++ game.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    let score = 0;
+
     let status = document.getElementById("status");
     let start = document.getElementById("start");
     let boundaries = document.getElementsByClassName("boundary");
+    let gameBorders = document.getElementById("game");
 
-    start.addEventListener("mouseenter", startGame);
+    console.log(start)
+
+    start.addEventListener("mouseenter", startGame);  
     
     function startGame(){
-        status.textContent = "Game Started"
+        status.textContent = "Game Started";
+        status.textContent = "Score: " + score;
         
         for(let i = 0; i < boundaries.length; i++){
             boundaries[i].style.backgroundColor = "#eeeeee";
@@ -14,6 +20,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         for(let i = 0; i < boundaries.length; i++){
             boundaries[i].addEventListener("mouseenter", lost);
+        }
+
+        gameBorders.addEventListener("mouseleave", outOfBounds)
+    }
+
+    function outOfBounds(){
+        status.textContent = "Stay within the borders please"
+        
+        for(let i = 0; i < boundaries.length; i++){
+            boundaries[i].style.backgroundColor = "red";
         }
     }
 
@@ -23,6 +39,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         for(let i = 0; i < boundaries.length; i++){
             boundaries[i].style.backgroundColor = "red";
         }
+
+        score--;
     }
 });
 

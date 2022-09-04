02a5b26e64134bed6779f5c7711b34b6edcd74ec
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-04 23:20:05 +0300

    Added greeting and saving the score option

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let score = 0;
+    let saveBool = false;
+    var name = prompt("What's your name?");
+
+    if(localStorage.getItem(name)){
+        score = localStorage.getItem(name)
+    }
 
     let status = document.getElementById("status");
     let start = document.getElementById("start");
@@ -11,8 +17,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
     showScore.textContent = "Score: " + score
     document.body.appendChild(showScore);
 
+    var greet = document.createElement("h2");
+    greet.textContent = "Hello,  " + name + "!"
+    document.body.appendChild(greet);
+
+    var saveScore = document.createElement("button");
+    saveScore.textContent = "Save Score"
+    document.body.appendChild(saveScore);
+
+    var removeScore = document.createElement("button");
+    removeScore.textContent = "Remove Score"
+    document.body.appendChild(removeScore);
+
     start.addEventListener("mouseenter", startGame);  
     start.addEventListener("click", restartGame);
+    saveScore.addEventListener("click", saveScoreLocally)
+    removeScore.addEventListener("click", removeScoreLocally);
     
     function startGame(){
         status.textContent = "Game Started";
@@ -65,11 +85,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     function endGame(){
+        if(saveBool){
+            localStorage.setItem(name, score);
+        }
         gameBorders.removeEventListener("mouseleave", outOfBounds)
         end.removeEventListener("mouseenter", wonGame)
         for(let i = 0; i < boundaries.length; i++){
             boundaries[i].removeEventListener("mouseenter", lostGame)
         }
+    }
+
+    function saveScoreLocally(){
+        saveBool = true;
+        localStorage.setItem(name, score);
+    }
+
+    function removeScoreLocally(){
+        saveBool = false;
+        localStorage.removeItem(name);
+        showScore.textContent = "Score: " + score
     }
 });
 
